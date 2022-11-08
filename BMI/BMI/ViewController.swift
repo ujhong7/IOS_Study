3588742ce6dede7402da26402b9bc446dde7b52f
yujaehong <>
2022-11-09 00:38:24 +0900

    no message

--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -60,7 +60,9 @@ class ViewController: UIViewController {
             let secondVC = segue.destination as! SecondViewController
             
             // 계산된 결과값을 다음화면에 전달
-            secondVC.bmi = self.bmi
+            secondVC.bmiNumber = self.bmi
+            secondVC.bmiColor = getBackgroundColor()
+            secondVC.adviceString = getBMIAdviceString()
             
             // 다음화면으로 가기전 텍스트필드 비우기
             heightTextField.text = ""
@@ -76,6 +78,44 @@ class ViewController: UIViewController {
         bmi = round(bmi*10)/10  // 반올림
         print("BMI결과값:\(bmi)")
         return bmi
+    }
+    
+    // 색깔 얻는 메서드
+    func getBackgroundColor() -> UIColor {
+        guard let bmi = bmi else { return UIColor.black }
+        switch bmi {
+        case ..<18.6:
+            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+        case 18.6..<23.0:
+            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+        case 23.0..<25.0:
+            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+        case 25.0..<30.0:
+            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+        case 30.0...:
+            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+        default:
+            return UIColor.black
+        }
+    }
+    
+    // 문자열 얻는 메서드
+    func getBMIAdviceString() -> String {
+        guard let bmi = bmi else { return "" }
+        switch bmi {
+        case ..<18.6:
+            return "저체중"
+        case 18.6..<23.0:
+            return "표준"
+        case 23.0..<25.0:
+            return "과체중"
+        case 25.0..<30.0:
+            return "중도비만"
+        case 30.0...:
+            return "고도비만"
+        default:
+            return ""
+        }
     }
     
 }
