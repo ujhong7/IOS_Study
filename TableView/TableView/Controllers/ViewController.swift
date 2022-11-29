0fac594734a613c16218f2543f15c4e8b94f9dd7
yujaehong <>
2022-11-29 22:19:41 +0900

    no message

--- TableView/TableView/Controllers/ViewController.swift
+++ TableView/TableView/Controllers/ViewController.swift
@@ -9,38 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 테이블뷰의 데이터를 표시하기 위한 배열
+    var moviesArray : [Movie] = []
+    // MVC패턴을 위한 데이터 매니저 (배열관리 - 데이터)
+    var movieDataManger = DataManger()
     
-    var moviesArray : [Movie] = [
-        Movie(movieImage: UIImage(named: "batman.png"), movieName: "배트맨", movieDescription: "배트맨이 출현하는 영화"),
-        Movie(movieImage: UIImage(named: "captain.png"), movieName: "캡틴 아메리카", movieDescription: "캡틴 아메리카의 기원. 캡틴 아메리카는 어떻게 탄생하게 되었을까?"),
-        Movie(movieImage: UIImage(named: "ironman.png"), movieName: "아이언맨", movieDescription: "토니 스타크가 출현, 아이언맨이 탄생하게된 계기가 재미있는 영화"),
-        Movie(movieImage: UIImage(named: "thor.png"), movieName: "토르", movieDescription: "아스가르드의 후계자 토르가 지구에 오게되는 스토리"),
-        Movie(movieImage: UIImage(named: "hulk.png"), movieName: "헐크", movieDescription: "브루스 배너 박사의 실험을 통해 헐크가 탄생하게 되는 영화"),
-        Movie(movieImage: UIImage(named: "spiderman.png"), movieName: "스파이더맨", movieDescription: "피터 파커 학생에서 스파이더맨이 되는 과정을 담은 스토리"),
-        Movie(movieImage: UIImage(named: "blackpanther.png"), movieName: "블랙펜서", movieDescription: "와칸다의 왕위 계승자 티찰과 블랙펜서가 되다."),
-        Movie(movieImage: UIImage(named: "doctorstrange.png"), movieName: "닥터스트레인지", movieDescription: "외과의사 닥터 스트레인지가, 히어로가 되는 과정을 담은 영화"),
-        Movie(movieImage: UIImage(named: "guardians.png"), movieName: "가디언즈 오브 갤럭시", movieDescription: "빌런 타노스에 맞서서 세상을 지키려는 가오겔 멤버들")
-    ]
-     
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 델리게이트 패턴의 대리자 설정
         tableView.dataSource = self
+        tableView.delegate = self
+        // 셀높이 설정
         tableView.rowHeight = 120
+        
+        title = "영화목록"
+        
+        // 일반적으로는 서버에 요청
+        movieDataManger.makeMovieData()
+        // 데이터 받아서 뷰컨의 배열에 저장
+        moviesArray = movieDataManger.getMovieData()
         
     }
 
 
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+        movieDataManger.updateMoiveData()   // 일반적으로는 서버에 요청 (데이터 업데이트)
+        moviesArray = movieDataManger.getMovieData()  // 다시 데이터 받아서 뷰컨의 배열에 저장
+        tableView.reloadData()   // 테이블뷰를 다시 불러오기
+    }
+    
+    
 }
 
 extension ViewController : UITableViewDataSource{
 
-
+    // 1) 테이블뷰에 몇개의 데이터를 표시할 것인지(셀이 몇개인지)를 뷰 컨트롤러에게 물어봄
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return moviesArray.count
     }
-
+    
+    // 2) 셀의 구성(셀에 표시하고자 하는 데이터 표시)을 뷰컨트롤러에게 물어봄
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
@@ -53,5 +65,26 @@ extension ViewController : UITableViewDataSource{
         return cell
     }
 
+}
 
+
+extension ViewController: UITableViewDelegate{
+    // 셀이 선택 되었때 어떤 동작을 할 것인지 뷰컨트롤러에게 물어봄
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 세그웨이 실행
+        performSegue(withIdentifier: "toDetail", sender: indexPath)
+        
+    }
+    // prepare세그웨이 (데이터전달)
+    override func prepare(for segue: UIStoryboardSegue, sender:Any?){
+        if segue.identifier == "toDetail"{
+            let detailVC = segue.destination as! DetailViewController
+            let array = movieDataManger.getMovieData()
+            let indexPath = sender as! IndexPath
+            
+            // 배열에서 아이템을 꺼내서, 다음화면으로 전달
+            detailVC.movieData = array[indexPath.row]
+        }
+    }
+    
 }
